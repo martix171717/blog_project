3c529ec124fc54c78e7b828b1d0bb8165546e2dd
Martyna Szymańska <martix171717@gmail.com>
2021-11-20 13:43:16 +0100

    Adding new post on blog

--- blog/__init__.py
+++ blog/__init__.py
@@ -1,3 +1,4 @@
+
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -9,3 +10,11 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 from blog import routes, models
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Entry": models.Entry
+    }
