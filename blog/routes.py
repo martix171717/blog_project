e04af604b46ec37482cb7ddfeaae89382a3954a9
Martyna Szymańska <martix171717@gmail.com>
2021-11-22 13:04:12 +0100

    Add login function

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,7 @@
-from flask import render_template, request, redirect, url_for, flash
+from flask import render_template, request, redirect, url_for, flash, session
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
 
 @app.route("/")
 def index():
@@ -48,3 +48,26 @@ def edit_entry(entry_id):
     else:
         errors = form.errors
     return render_template("entry_form.html", form=form, errors=errors)
+
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
