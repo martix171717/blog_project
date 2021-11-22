a8c1626248e173e2307885876a6ba2433592506b
Martyna Szymańska <martix171717@gmail.com>
2021-11-22 13:37:02 +0100

    Add login logout function

--- blog/routes.py
+++ blog/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, request, redirect, url_for, flash, session
 from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm, LoginForm
+import functools
 
 @app.route("/")
 def index():
@@ -26,7 +27,17 @@ def create_edit_entry(form, entry_id=None, entry=None):
     else:
         return form.errors
 
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
+
+
 @app.route("/new-post/", methods=["GET", "POST"])
+@login_required
 def create_entry():
     form = EntryForm()
     errors = None
@@ -38,6 +49,7 @@ def create_entry():
     return render_template("entry_form.html", form=form, errors=errors)
 
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def edit_entry(entry_id):
     entry = Entry.query.filter_by(id=entry_id).first_or_404()
     form = EntryForm(obj=entry)
@@ -58,7 +70,7 @@ def login():
         if form.validate_on_submit():
             session['logged_in'] = True
             session.permanent = True  # Use cookie to store session.
-            flash('You are now logged in.', 'success')
+            flash('Zostałeś poprawnie zalogowany!', 'success')
             return redirect(next_url or url_for('index'))
         else:
             errors = form.errors
@@ -69,5 +81,6 @@ def login():
 def logout():
     if request.method == 'POST':
         session.clear()
-        flash('You are now logged out.', 'success')
+        flash('Zostałeś poprawnie wylogowany!', 'success')
     return redirect(url_for('index'))
+
--- config.py
+++ config.py
@@ -10,5 +10,5 @@ class Config:
    )
    SQLALCHEMY_TRACK_MODIFICATIONS = False
    
-ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
-ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "change-me")
+   ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "admin")
+   ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "topsecret")
